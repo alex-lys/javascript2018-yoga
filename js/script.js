a821a323e30de5f5e7cd3f7bac48963289e984ad
Alex <la.prof@ya.ru>
2019-09-23 19:08:37 +0300

    add ajax forms

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', function() {
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
+    const hideTabContent = (a) => {
         for (let i = a; i < tabContent.length; i += 1) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideTabContent(1);
 
-    function showTabcontent(b) {
+    const showTabcontent = (b) => {
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
@@ -38,9 +38,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // timer
 
-    let deadline = '2019-09-16';
+    let deadline = '2019-10-16';
 
-    function getTimeRemaining(endtime) {
+    const getTimeRemaining = (endtime) => {
         let total = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((total / 1000) % 60),
             minutes = Math.floor((total / 1000/ 60) % 60),
@@ -65,14 +65,13 @@ window.addEventListener('DOMContentLoaded', function() {
         };
     }
 
-    function setClock(id, endtime) {
+    const setClock = (id, endtime) => {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            interval = setInterval(updateClock, 1000);
+            seconds = timer.querySelector('.seconds');
 
-        function updateClock() {
+        const updateClock = () => {
             let timeData = getTimeRemaining(endtime);
             hours.textContent = timeData.hours;
             minutes.textContent = timeData.minutes;
@@ -83,31 +82,105 @@ window.addEventListener('DOMContentLoaded', function() {
 
             }
         }
+
+        let interval = setInterval(updateClock, 1000);
+        
     }
     
     setClock('timer', deadline);
 
     //modal
 
-    let more = document.querySelectorAll('.more, .description-btn'),
+    let more = document.querySelector('.more'),
+        moreTab = [...document.querySelectorAll('.description-btn')],
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
-    console.log(more);
         
-    more.forEach(function(element) {
+    const requestModal = (element) => {
         element.addEventListener('click', function() {
             overlay.style.display = 'block';
-            console.log(this);
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
         })
+    
+        close.addEventListener('click', function() {
+            overlay.style.display = 'none';
+            element.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
+    };
+
+    requestModal(more);
+    moreTab.forEach(function (element) {
+        requestModal(element);
     });
 
-    close.addEventListener('click', function() {
-        overlay.style.display = 'none';
-        more.forEach(function(element) {
-            element.classList.remove('more-splash');
-        })
-        document.body.style.overflow = '';
-    });
+    //ajax forms
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, мы скоро свяжемся с вами!',
+        failure: 'Что-то пошло не так'
+    };
+
+    let form = document.querySelector('.main-form'),
+        formBottom = document.querySelector('#form'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+
+    const sendForm = (element) => {
+        element.addEventListener('submit', function(event) {
+            event.preventDefault();
+            element.appendChild(statusMessage);
+
+            let formData = new FormData(element);
+            let convertedData = {};
+            formData.forEach(function(key, value) {
+                convertedData[key] = value;
+            });
+            let json = JSON.stringify(convertedData);
+    
+            const postData = data => {
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                    request.send(data);
+            
+                    request.addEventListener('readystatechange', function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                })
+            };
+            
+            let inputForm = element.getElementsByTagName('input');
+            const clearInput = () => {
+                for (let i = 0; i < inputForm.length; i += 1) {
+                    inputForm[i].value = '';
+                }
+            };
+
+            postData(json)
+                .then(() => {
+                    statusMessage.innerHTML = message.loading;
+                })
+                .then(() => {
+                    statusMessage.innerHTML = message.success;
+                })
+                .catch(() => {
+                    statusMessage.innerHTML = message.failure;
+                })
+                .then(clearInput)
+        });
+    };
+    sendForm(form);
+    sendForm(formBottom);
+
 });
